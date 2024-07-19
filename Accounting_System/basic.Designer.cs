0adeaaf41706a878cd0879607216af5be8528434
Mohamed Ahmed <mohamed.5580.558@gmail.com>
2024-07-19 14:38:27 +0300

    uploud

---
+++ Accounting_System/basic.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Accounting_System
+{
+    partial class basic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // basic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 450);
+            this.Name = "basic";
+            this.Text = "basic";
+            this.Load += new System.EventHandler(this.basic_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
